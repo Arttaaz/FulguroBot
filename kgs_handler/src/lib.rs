3621d48df0cd2fc64d6d05e0fb64255eb025b329
theo barollet <theo.barollet@grenoble-inp.org>
2019-06-25 20:43:15 +0200

    quickrun of rustfmt

--- kgs_handler/src/lib.rs
+++ kgs_handler/src/lib.rs
@@ -16,33 +16,33 @@ pub struct User {
 }
 
 impl Client {
-    pub fn start(username:String, password:String) -> Client {
+    pub fn start(username: String, password: String) -> Client {
         let user = User {
-            username: username,
-            password : password,
-            rank : String::from("U"),
-            flags : String::from(""),
+            username,
+            password,
+            rank: String::from("U"),
+            flags: String::from(""),
         };
 
-        let client = Client {
-            user: user,
-        };
+        let client = Client { user: user };
 
         client
     }
     pub fn login(&self) {
         let mut data = HashMap::new();
 
-        data.insert("type","LOGIN");
-        data.insert("name",&self.user.username);
-        data.insert("password",&self.user.password);
-        data.insert("locale","fr_FR");
+        data.insert("type", "LOGIN");
+        data.insert("name", &self.user.username);
+        data.insert("password", &self.user.password);
+        data.insert("locale", "fr_FR");
 
         let client = reqwest::Client::new();
-        let mut res = client.post("localhost::8080/access")
-        .json(&data)
-        .send().expect("Could not login");
+        let mut res = client
+            .post("localhost::8080/access")
+            .json(&data)
+            .send()
+            .expect("Could not login");
 
-        println!("{}",res.text().expect("Unvalid response text"));
+        println!("{}", res.text().expect("Unvalid response text"));
     }
 }
